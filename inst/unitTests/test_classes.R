af4f1fa6cff642e6a0c8c9a85a728ced7365b56c
tgirke <tgirke@citrus.ucr.edu>
2015-12-17 13:30:47 -0800

    some edits

--- inst/unitTests/test_classes.R
+++ inst/unitTests/test_classes.R
@@ -1,6 +1,6 @@
 samplepath <- system.file("extdata", "Whole_Blood_Analysis.snpgenes.head100", package="longevityTools")
 test.import <- function(){
-	## Test import of eQTL file
-	dat <- read.delim(samplepath)
+    ## Test import of eQTL file
+    dat <- read.delim(samplepath)
     checkTrue(nrow(dat)==100)
 }
