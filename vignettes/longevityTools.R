770404ede1a893fd6cd7ef12978f0ba140effb2a
Thomas Girke <tgirke@citrus.ucr.edu>
2015-12-16 16:45:13 -0800

    some edits

---
+++ vignettes/longevityTools.R
@@ -0,0 +1,30 @@
+## ----style, echo = FALSE, results = 'asis'-------------------------------
+BiocStyle::markdown()
+options(width=100, max.print=1000)
+knitr::opts_chunk$set(
+    eval=as.logical(Sys.getenv("KNITR_EVAL", "TRUE")),
+    cache=as.logical(Sys.getenv("KNITR_CACHE", "TRUE")))
+
+## ----setup, echo=FALSE, messages=FALSE, warnings=FALSE-------------------
+suppressPackageStartupMessages({
+    library(longevityTools)
+    library(ggplot2)
+})
+
+## ----install, eval=FALSE-------------------------------------------------
+## source("http://bioconductor.org/biocLite.R") # Sources the biocLite.R installation script
+## biocLite("tgirke/longevityTools", build_vignettes=TRUE, dependencies=TRUE) # Installs package from GitHub
+
+## ----documentation, eval=FALSE-------------------------------------------
+## library("longevityTools") # Loads the package
+## library(help="longevityTools") # Lists package info
+## vignette("longevityTools") # Opens vignette
+
+## ----genRna_workflow, eval=FALSE-----------------------------------------
+## library(systemPipeRdata)
+## genWorkenvir(workflow="rnaseq")
+## setwd("rnaseq")
+
+## ----sessionInfo---------------------------------------------------------
+sessionInfo()
+
