a8507998a485bbfcfbbde933ea0fbd70cfd770e4
DanielEvansLab <danev1@gmail.com>
2016-02-09 01:06:57 -0800

    some edits

--- R/longevityTools_eQTL.R
+++ R/longevityTools_eQTL.R
@@ -6,7 +6,7 @@
 geneGrep <- function(dat,genes){
   if(sum(dat$gene_name %in% genes)>0){
     myRows <- which(dat$gene_name %in% genes)
-    dat2 <- dat[myRows, c("snp","beta", "p_value", "ref", "alt", "gene_name")]
+    dat2 <- dat[myRows, c("snp","beta", "p_value", "ref", "alt", "gene_name","nom_thresh","snp_chrom","snp_pos","snp_id_1kg_project_phaseI_v3","rs_id_dbSNP142_GRCh37p13")]
     return(dat2)
   } else {
     print("No matches")
@@ -17,4 +17,16 @@ geneGrep <- function(dat,genes){
 # dat <- read.table("~/Documents/longevity/GTEx/data/Whole_Blood_Analysis.snpgenes.head100", header=TRUE, sep="\t", stringsAsFactors=FALSE)
 # myGenes<- c("RP11-693J15.4", "RP11-809N8.4", "junkNoMatch")
 # result <- geneGrep(dat, myGenes)
+
+filter_1KGsamples <- function(pop,ped_file,pop_file){
+  if(pop %in% c("AFR","AMR","EAS","EUR","SAS")){
+    pop_file <- pop_file[pop_file$super_pop==pop,]
+    ped_file <- ped_file[ped_file$related.genotypes==0,]
+    result<-merge(ped_file,pop_file,by.x="Individual.ID",by.y="sample")
+    return(result)
+  } else {
+    print(paste(pop, "not found in pop_file."))
+  }
+}
+
 
