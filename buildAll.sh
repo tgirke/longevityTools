762e604a25fd59caebcfc155bcf6efb290bbe644
tgirke <tgirke@citrus.ucr.edu>
2016-02-15 12:17:35 -0800

    some edits

--- buildAll.sh
+++ buildAll.sh
@@ -3,9 +3,9 @@
 ## Author: Thomas Girke
 ## Last update: Feb 9, 2016
 
-## (1) All changes should be made from master branch. Note, the script
+## (1) All changes should be made from gh_pages_source branch. Note, the script
 ##     should be run from the root directory of the repo.
-git checkout master  
+git checkout gh_pages_source  
 
 ## (2) Edit existing pages and/or add new ones, usually in ./mydoc
 ## No automation for this one ... :).
@@ -20,19 +20,19 @@ echo "done"
 echo "Finished building all the web outputs!!!"
 echo "Now the builds are committed and pushed to github."
 
-## (4) Commit edits made in master branch (assumes you have added/deleted files as needed)
+## (4) Commit edits made in gh_pages_source branch (assumes you have added/deleted files as needed)
 git add -A :/
 git commit -am "some edits"
-git push -u origin master
-echo "Committed/pushed changes to master branch on GitHub"
+git push -u origin gh_pages_source
+echo "Committed/pushed changes to gh_pages_source branch on GitHub"
 
 ## (5) Sync changes to gh-pages branch
 git checkout gh-pages
-rsync -avh --stats ~/Dropbox/Websites/doc_outputs/mydoc/designers/ ~/Dropbox/Websites/manuals/t add .
+rsync -avh --stats ../doc_outputs/mydoc/designers/ .
 clear
 git add -A :/
 git commit -am "some edits"; git push -u origin gh-pages
 echo "Committed/pushed changes to gh-pages branch on GitHub"
-git checkout master 
-echo "Switched back to master branch on local repo."
+git checkout gh_pages_source 
+echo "Switched back to gh_pages_source branch on local repo."
 
